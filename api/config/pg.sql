1b3e5224c9d457d394b3b2c4b9fd05da01c42d9e
jpfalcuci <jpfalcuci@gmail.com>
2024-06-13 20:59:01 -0300

    feat: sql queries

--- api/config/pg.sql
+++ api/config/pg.sql
@@ -26,3 +26,22 @@ insert into tb_categoria (descricao) values (
 	('Brinquedos'),
 	('Outros')
 )
+
+create table tb_author (
+  codigo serial,
+  nome varchar(100) not null,
+  biografia text,
+  data_nascimento date,
+  primary key (codigo)
+);
+
+create table tb_book (
+  codigo serial,
+  titulo varchar(150) not null,
+  id_autor INT not null,
+  data_publicacao date,
+  isbn varchar(20),
+  resumo text,
+  primary key (codigo),
+  foreign key (id_autor) references tb_author(codigo)
+);
