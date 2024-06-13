6615df4cac4542d8552deadd174b9e323293ebe0
jpfalcuci <jpfalcuci@gmail.com>
2024-06-13 20:56:55 -0300

    fix: db fields

--- api/api/v2/author/author-model.js
+++ api/api/v2/author/author-model.js
@@ -7,16 +7,20 @@ const Author = database.sequelize.define('Author', {
     autoIncrement: true,
     allowNull: false,
     primaryKey: true,
+    field: 'codigo'
   },
   name: {
     type: Sequelize.STRING,
     allowNull: false,
+    field: 'nome'
   },
   biography: {
     type: Sequelize.TEXT,
+    field: 'biografia'
   },
   birthDate: {
     type: Sequelize.DATE,
+    field: 'data_nascimento'
   },
 }, {
   timestamps: false,
